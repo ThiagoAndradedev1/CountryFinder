e9a4667c76117b83ac8a7716a7f6c807fd7a554f
Thiago <thiago.andrade.dev1@gmail.com>
2021-10-16 14:11:58 -0300

    Adicionando a página home a aplicação

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CountriesList from "../countries/CountriesList";
+
+const Home = () => {
+  return (
+    <>
+      <CountriesList />
+    </>
+  );
+};
+
+export default Home;
