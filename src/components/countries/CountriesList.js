f91aa4679a661c37180e1a9629f9ecdee3272fcd
Thiago <thiago.andrade.dev1@gmail.com>
2021-10-16 15:32:36 -0300

    Adicionando componente CountriesList

--- src/components/countries/CountriesList.js
+++ src/components/countries/CountriesList.js
@@ -1,35 +1,24 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
-
-const GET_COUNTRIES = gql`
-  query {
-    Country {
-      _id
-      capital
-      name
-      flag {
-        svgFile
-      }
-      topLevelDomains {
-        name
-      }
-    }
-  }
-`;
+import { Link } from "react-router-dom";
+import { useCountries } from "../../hooks/useCountries";
 
 const CountriesList = () => {
-  const { error, loading, data } = useQuery(GET_COUNTRIES);
+  const { error, loading, data } = useCountries();
 
   if (loading) return <div>Spinner...</div>;
 
   if (error) return <div>Something went wrong...</div>;
 
-  console.log(data.Country[0]);
+  console.log(data);
 
   return (
     <div>
       {data.Country.map((ctr) => {
-        return <h1>{ctr.name}</h1>;
+        return (
+          <Link to={`/${ctr._id}`}>
+            <h1 key={ctr._id}>{ctr.name}</h1>;
+          </Link>
+        );
       })}
     </div>
   );
