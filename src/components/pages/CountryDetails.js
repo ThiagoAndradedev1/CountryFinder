6af1c87c419b4ded557b69dca870c2416df19cea
Thiago <thiago.andrade.dev1@gmail.com>
2021-10-16 15:32:10 -0300

    Adicionando a página de CountryDetails

---
+++ src/components/pages/CountryDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Country from "../countries/Country";
+
+const CountryDetails = () => {
+  return (
+    <>
+      <Country />
+    </>
+  );
+};
+
+export default CountryDetails;
