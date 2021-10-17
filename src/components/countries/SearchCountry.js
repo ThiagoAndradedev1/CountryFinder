d04b331c544bea1b86f26201de42f264e2eeeae7
Thiago <thiago.andrade.dev1@gmail.com>
2021-10-17 16:05:42 -0300

    Alterações na lógica do componente SearchCountry

--- src/components/countries/SearchCountry.js
+++ src/components/countries/SearchCountry.js
@@ -1,25 +1,227 @@
-import React, { useState } from "react";
-import { useSearchCountry } from "../../hooks/useSearchCountry";
+import { useApolloClient } from "@apollo/client";
+import React, { Fragment, useState } from "react";
+import {
+  GET_COUNTRY_BY_NAME,
+  useSearchCountry,
+} from "../../hooks/useSearchCountry";
+import { convertToPascalCase } from "../../utils/utils";
 
 const SearchCountry = () => {
   const [name, setName] = useState("");
 
-  const { data, loading, error, getCountry } = useSearchCountry();
+  const [searchError, setSearchError] = useState(false);
 
-  console.log(data);
+  const [country, setCountry] = useState("");
+  const [capital, setCapital] = useState("");
+  const [area, setArea] = useState("");
+  const [population, setPopulation] = useState("");
+  const [topLevelDomain, setTopLevelDomain] = useState("");
 
-  if (loading) return <div>Spinner...</div>;
+  const [openForm, setOpenForm] = useState(false);
 
-  if (error) return <div>Something went wrong...</div>;
+  const { data, loading, getCountry } = useSearchCountry();
+
+  const client = useApolloClient();
+
+  const cleanState = () => {
+    setCountry("");
+    setCapital("");
+    setArea("");
+    setPopulation("");
+    setTopLevelDomain("");
+  };
+  const openFormInfo = () => {
+    setOpenForm(!openForm);
+    cleanState();
+  };
+
+  const searchCountryInfo = (e) => {
+    e.preventDefault();
+    setOpenForm(false);
+    if (name !== "") {
+      getCountry({ variables: { name: convertToPascalCase(name).trim() } });
+      cleanState();
+      setSearchError(false);
+    } else {
+      setSearchError(true);
+    }
+  };
+
+  const modifyCache = () => {
+    console.log(population);
+    client.writeQuery({
+      query: GET_COUNTRY_BY_NAME,
+      variables: { name },
+      data: {
+        Country: [
+          {
+            _id: data.Country[0]._id,
+            capital: !!capital ? capital : data.Country[0].capital,
+            name: !!country ? country : data.Country[0].name,
+            area: !!area ? area : data.Country[0].area,
+            population: !!population ? population : data.Country[0].population,
+            flag: {
+              svgFile: data.Country[0].flag.svgFile,
+              __typename: "Flag",
+            },
+            topLevelDomains: [
+              {
+                name: !!topLevelDomain
+                  ? topLevelDomain
+                  : data.Country[0].topLevelDomains[0].name,
+                __typename: "TopLevelDomain",
+              },
+            ],
+            __typename: "Country",
+          },
+        ],
+      },
+    });
+  };
 
   return (
-    <div>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button onClick={() => getCountry({ variables: { name } })}>
-        Search
-      </button>
-      <h1>{data?.Country[0].capital}</h1>
-    </div>
+    <>
+      <form className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Pesquisar por países..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button
+          type="submit"
+          onClick={(e) => searchCountryInfo(e)}
+          className="btn btn-dark-blue btn-block"
+        >
+          <i className="fas fa-search mr-5"></i> Pesquisar
+        </button>
+      </form>
+      {loading && <h1>Loading...</h1>}
+      {searchError && <h1>Você precisa digitar algo...</h1>}
+      {data?.Country.length === 0 && <h1>Something went wrong...</h1>}
+      {data?.Country.length > 0 && (
+        <div className="fade-in">
+          <button
+            onClick={() => openFormInfo()}
+            className="btn btn-light btn-block mt-10"
+          >
+            <i className="far fa-edit mr-5"></i> Editar Informações
+          </button>
+          <div className="card grid-2">
+            <div className="all-center">
+              <img
+                src={data?.Country[0].flag.svgFile}
+                className="round-img widthImgAvatar"
+                alt="avatarurl"
+                width="150px"
+              />
+            </div>
+            <ul>
+              <li>
+                <Fragment>
+                  <strong>País: </strong> {data?.Country[0].name}
+                </Fragment>
+              </li>
+              <li>
+                <Fragment>
+                  <strong>Capital: </strong> {data?.Country[0].capital}
+                </Fragment>
+              </li>
+              <li>
+                <Fragment>
+                  <strong>Área: </strong> {data?.Country[0].area}
+                </Fragment>
+              </li>
+              <li>
+                <Fragment>
+                  <strong>População: </strong> {data?.Country[0].population}
+                </Fragment>
+              </li>
+              <li>
+                <Fragment>
+                  <strong>Top Level Domain: </strong>{" "}
+                  {data?.Country[0].topLevelDomains[0].name}
+                </Fragment>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+      {openForm && data && (
+        <div className="fade-in">
+          <h1 className="text-center">
+            <i className="far fa-edit mr-5 mt-30"></i> Editar Informações
+          </h1>
+          <div className="card grid-2">
+            <div>
+              <h4>País:</h4>
+              <input
+                type="text"
+                name="text"
+                placeholder="Alterar país..."
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+              />
+            </div>
+            <div>
+              <h4>Capital:</h4>
+              <input
+                type="text"
+                name="text"
+                placeholder="Alterar capital..."
+                value={capital}
+                onChange={(e) => setCapital(e.target.value)}
+              />
+            </div>
+            <div>
+              <h4>Área:</h4>
+              <input
+                type="text"
+                name="text"
+                placeholder="Alterar área..."
+                value={area}
+                onChange={(e) => setArea(e.target.value)}
+              />
+            </div>
+            <div>
+              <h4>População:</h4>
+              <input
+                type="text"
+                name="text"
+                placeholder="Alterar população..."
+                value={population}
+                onChange={(e) => setPopulation(e.target.value)}
+              />
+            </div>
+            <div>
+              <h4>Top Level Domain:</h4>
+              <input
+                type="text"
+                name="text"
+                placeholder="Alterar Top Level Domain..."
+                value={topLevelDomain}
+                onChange={(e) => setTopLevelDomain(e.target.value)}
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            onClick={() => modifyCache()}
+            className="btn btn-dark-blue btn-block"
+          >
+            <i className="fas fa-pencil-alt mr-5"></i> Editar
+          </button>
+          <button
+            type="submit"
+            onClick={() => openFormInfo()}
+            className="btn btn-danger btn-block mt-15"
+          >
+            <i className="far fa-times-circle mr-5"></i> Fechar
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
