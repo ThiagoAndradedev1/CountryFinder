285b46810937c07357ec2db50347dec19a9fea0d
Thiago <thiago.andrade.dev1@gmail.com>
2021-10-17 20:27:49 -0300

    Adiciona link para home na page de NotFound

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <>
-      <h1 className="mt-10 text-center">Essa rota não existe!</h1>
+      <h1 className="mt-10 text-center">Essa rota não existe! :(</h1>
+      <Link to="/">
+        <h3 className="text-center">Voltar para a página principal</h3>
+      </Link>
     </>
   );
 };
