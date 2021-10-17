05a4771d4d6bf10f3e023596d7ea34b6fad0b508
Thiago <thiago.andrade.dev1@gmail.com>
2021-10-17 12:23:32 -0300

    Criando o design do componente de CountriesList

--- src/components/countries/CountriesList.js
+++ src/components/countries/CountriesList.js
@@ -15,24 +15,23 @@ const CountriesList = () => {
   return (
     <Fragment>
       {/* <img src={git} className="gitImg" alt="usergit" /> */}
-      <h1 className="text-center pb-10">CONHEÇA MAIS DO MUNDO!</h1>
+      {/* <h1 className="text-center pb-10">CONHEÇA MAIS DO MUNDO!</h1> */}
+      <h1>Home</h1>
       <div className="grid-3">
         {data.Country.map((ctr) => {
           return (
-            <Link to={`/${ctr._id}`}>
+            <Link to={`/details/${ctr._id}`}>
               <div key={ctr._id} className="card text-center container">
-                <div className="all-center">
+                {/* <div className="all-center">
                   <img
                     src={ctr.flag.svgFile}
                     className="round-img widthImgAvatar"
                     alt="avatarurl"
                     width="150px"
                   />
-                </div>
-                <h1 className="text-success nameStyle text-center">
-                  {ctr.name}
-                </h1>
-                <h1 className="text-success nameStyle text-center">
+                </div> */}
+                <h1 className="text-dark nameStyle text-center">{ctr.name}</h1>
+                <h1 className="text-dark nameStyle text-center">
                   {ctr.capital}
                 </h1>
               </div>
