09fde6d2622ec7bdd2782b1e9dfa5ea3f940fb2a
Thiago <thiago.andrade.dev1@gmail.com>
2021-10-17 11:05:18 -0300

    Adicionando informações em CountriesList

--- src/components/countries/CountriesList.js
+++ src/components/countries/CountriesList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Fragment } from "react/cjs/react.production.min";
 import { useCountries } from "../../hooks/useCountries";
 
 const CountriesList = () => {
@@ -12,15 +13,34 @@ const CountriesList = () => {
   console.log(data);
 
   return (
-    <div>
-      {data.Country.map((ctr) => {
-        return (
-          <Link to={`/${ctr._id}`}>
-            <h1 key={ctr._id}>{ctr.name}</h1>;
-          </Link>
-        );
-      })}
-    </div>
+    <Fragment>
+      {/* <img src={git} className="gitImg" alt="usergit" /> */}
+      <h1 className="text-center pb-10">CONHEÇA MAIS DO MUNDO!</h1>
+      <div className="grid-3">
+        {data.Country.map((ctr) => {
+          return (
+            <Link to={`/${ctr._id}`}>
+              <div key={ctr._id} className="card text-center container">
+                <div className="all-center">
+                  <img
+                    src={ctr.flag.svgFile}
+                    className="round-img widthImgAvatar"
+                    alt="avatarurl"
+                    width="150px"
+                  />
+                </div>
+                <h1 className="text-success nameStyle text-center">
+                  {ctr.name}
+                </h1>
+                <h1 className="text-success nameStyle text-center">
+                  {ctr.capital}
+                </h1>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </Fragment>
   );
 };
 
