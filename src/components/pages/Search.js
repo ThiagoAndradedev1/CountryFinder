229252e61a14af623fd29ef666c02f9099affaea
Thiago <thiago.andrade.dev1@gmail.com>
2021-10-17 12:17:47 -0300

    Adicionando página de Search

---
+++ src/components/pages/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchCountry from "../countries/SearchCountry";
+
+const Search = () => {
+  return (
+    <>
+      <SearchCountry />
+    </>
+  );
+};
+
+export default Search;
