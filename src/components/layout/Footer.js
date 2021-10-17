7e3bc60b650bc9c122e65a353915fc07b454bde2
Thiago <thiago.andrade.dev1@gmail.com>
2021-10-17 12:20:47 -0300

    Adicionando componente Footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer pb-40">
+      <p className="footerTextSize">
+        Made with <i className="fas fa-heart text-danger"></i> by
+        @ThiagoAndradedev1
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
