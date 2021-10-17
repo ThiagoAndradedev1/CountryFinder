dcfd15fc00cb041c53fd8bc2fa9fa4c3916c5406
Thiago <thiago.andrade.dev1@gmail.com>
2021-10-17 20:02:40 -0300

    Adicionando arquivo constants

---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+const ERROR_REQUEST = "Algo deu errado...";
+const ERROR_INVALID_EMAIL_AND_PASSWORD =
+  "A sua senha ou email são invalidos...";
+const ERROR_EMPTY_SEARCH = "Você precisa digitar algo...";
+
+export { ERROR_REQUEST, ERROR_INVALID_EMAIL_AND_PASSWORD, ERROR_EMPTY_SEARCH };
